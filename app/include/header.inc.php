62ae2e42f2f1d1834a7a5debc3e315860d39cd8f
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-02-21 22:36:32 +0100

    new files

---
+++ app/include/header.inc.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="shop.php">Webshop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="shop.php">Shop</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="shopping-cart.php">Einkaufswagen</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="account.php">Mein Profil</a>
+        </li>
+    </ul>
+  </div>
+</nav>
---
+++ doku.md
@@ -0,0 +1,5 @@
+# This is a brief developer documentation
+
+### Important links
+
+* [Bootstrap](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
