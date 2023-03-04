dca6de8776f7dedbeca7beab5bfa98a34c25d971
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-03-04 09:11:14 +0100

    navbar

--- app/universal/header.inc.php
+++ app/universal/header.inc.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="shop.php">Shop</a>
+            <a class="nav-link" href="index.php">Shop</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="shopping-cart.php">Einkaufswagen</a>
