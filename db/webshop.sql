05543e42c334b7362460065c82e806324b71f8c4
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-03-09 14:47:39 +0100

    Update database

--- db/webshop.sql
+++ db/webshop.sql
@@ -34,7 +34,7 @@ create table orders (
     user_id int(11) not null,
     product_id int(11) not null,
     quantity int(11) not null,
-    
+
     primary key (id),
     foreign key (user_id) references users(id),
     foreign key (product_id) references products(id)
