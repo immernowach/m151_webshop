19ea4d062e21d15aa7e8c032469f49c0595d21bf
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-03-09 14:18:24 +0100

    Update database and structure

--- db/webshop.sql
+++ db/webshop.sql
@@ -3,16 +3,39 @@ use 151_webshop;
 
 create table users (
     id int(11) not null auto_increment,
-    username varchar(255) not null,
-    password varchar(255)
     email varchar(255),
-    phone varchar(255),
+    password varchar(255),
 
-    lastname varchar(255),
-    firstname varchar(255),
+    primary key (id)
+);
+
+create table locations (
+    id int(11) not null auto_increment,
     address varchar(255),
-    city varchar(255),
+    city_name varchar(255),
     zip varchar(255),
     country varchar(255),
+
+    primary key (id),
+    foreign key (id) references users(id)
+);
+
+create table products (
+    id int(11) not null auto_increment,
+    name varchar(255) not null,
+    description varchar(255),
+    price decimal(10,2),
+
     primary key (id)
+);
+
+create table orders (
+    id int(11) not null auto_increment,
+    user_id int(11) not null,
+    product_id int(11) not null,
+    quantity int(11) not null,
+    
+    primary key (id),
+    foreign key (user_id) references users(id),
+    foreign key (product_id) references products(id)
 );
