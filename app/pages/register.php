89fc850e6272c0c0f3d097fe8013926cff8e20a6
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-03-09 16:01:12 +0100

    Update registration

--- app/pages/register.php
+++ app/pages/register.php
@@ -15,97 +15,44 @@
 
         // Initialisierung
 $error = $message =  '';
-$firstname = $lastname = $email = $username = '';
+$firstname = $lastname = $email = '';
 
-// Wurden Daten mit "POST" gesendet?
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-  // Ausgabe des gesamten $_POST Arrays
-  echo "<pre>";
-  print_r($_POST);
-  echo "</pre>";
 
-  // vorname ausgefüllt?
-  if(isset($_POST['firstname'])){
-    //trim and sanitize
-    $firstname = trim(htmlspecialchars($_POST['firstname']));
-    
-    //mindestens 1 Zeichen und maximal 30 Zeichen lang
-    if(empty($firstname) || strlen($firstname) > 30){
-      $error .= "Geben Sie bitte einen korrekten Vornamen ein.<br />";
-    }
-  } else {
-    $error.= "Geben Sie bitte einen Vornamen ein.<br />";
-  }
-
-  // nachname ausgefüllt?
-  if(isset($_POST['lastname'])){
-    //trim and sanitize
-    $lastname = trim(htmlspecialchars($_POST['lastname']));
-    
-    //mindestens 1 Zeichen und maximal 30 Zeichen lang
-    if(empty($lastname) || strlen($lastname) > 30){
-      $error .= "Geben Sie bitte einen korrekten Nachname ein.<br />";
-    }
-  } else {
-    $error.= "Geben Sie bitte einen Nachname ein.<br />";
-  }
-  
-  // email ausgefüllt?
   if(isset($_POST['email'])){
-    //trim
     $email = trim($_POST['email']);
-    
-    //mindestens 1 Zeichen und maximal 100 Zeichen lang, gültige Emailadresse
+
     if(empty($email) || strlen($email) > 100 || filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-      $error .= "Geben Sie bitte eine korrekten Emailadresse ein.<br />";
+      $error .= "Geben Sie bitte eine korrekte Emailadresse ein.<br />";
     }
   } else {
     $error.= "Geben Sie bitte eine Emailadresse ein.<br />";
   }
 
-  // username ausgefüllt?
-  if(isset($_POST['username'])){
-    //trim and sanitize
-    $username = trim($_POST['username']);
-    
-    //mindestens 1 Zeichen , entsprich RegEX
-    if(empty($username) || !preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,30}/", $username)){
-      $error .= "Geben Sie bitte einen korrekten Usernamen ein.<br />";
-    }
-  } else {
-    $error.= "Geben Sie bitte einen Username ein.<br />";
-  }
-
-  // passwort ausgefüllt
   if(isset($_POST['password'])){
-    //trim and sanitize
-    $password = trim($_POST['password']);
-
+    $password = trim($_POST['password']); //trim and sanitize
     $password = password_hash($password, PASSWORD_DEFAULT);
-    
-    //mindestens 1 Zeichen , entsprich RegEX
+
     if(empty($password) || !preg_match("/(?=^.{8,255}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/", $password)){
       $error .= "Geben Sie bitte einen korrektes Password ein.<br />";
     }
   } else {
     $error.= "Geben Sie bitte ein Password ein.<br />";
   }
-
-  // wenn kein Fehler vorhanden ist, schreiben der Daten in die Datenbank
-  if(empty($error)){
-    // INPUT Query erstellen, welches firstname, lastname, username, password, email in die Datenbank schreibt
-    $query = "INSERT INTO users (firstname, lastname, username, password, email) VALUES (?, ?, ?, ?, ?)";
+  
+  if(empty($error)) { // wenn kein Fehler vorhanden ist, schreiben der Daten in die Datenbank
+    // INPUT Query erstellen, welches password, email in die Datenbank schreibt
+    $query = "INSERT INTO users (password, email) VALUES (?, ?)";
     // Query vorbereiten mit prepare();
     $stmt = $mysqli->prepare($query);
     // Parameter an Query binden mit bind_param();
-    $stmt->bind_param("sssss", $firstname, $lastname, $username, $password, $email);
+    $stmt->bind_param("ss", $password, $email);
     // query ausführen mit execute();
     $stmt->execute();
     // Verbindung schliessen
     $stmt->close();
-    // Weiterleitung auf login.php
-    $message = "Sie wurden erfolgreich registriert. Sie können sich nun einloggen.";
-    header("Location: 5_login.php");
+    $message = "Sie wurden erfolgreich registriert.";
+    header('Location: account.php');
   }
   }
     ?>
@@ -126,24 +73,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         }
       ?>
       <form action="" method="post">
-        <!-- vorname -->
-        <div class="form-group">
-          <label for="firstname">Vorname *</label>
-          <input type="text" name="firstname" class="form-control" id="firstname"
-            value="<?php echo $firstname ?>"
-            placeholder="Geben Sie Ihren Vornamen an."
-            maxlength="30"
-            required="true">
-        </div>
-        <!-- nachname -->
-        <div class="form-group">
-          <label for="lastname">Nachname *</label>
-          <input type="text" name="lastname" class="form-control" id="lastname"
-            value="<?php echo $lastname ?>"
-            placeholder="Geben Sie Ihren Nachnamen an"
-            maxlength="30"
-            required="true">
-        </div>
         <!-- email -->
         <div class="form-group">
           <label for="email">Email *</label>
@@ -151,17 +80,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
             value="<?php echo $email ?>"
             placeholder="Geben Sie Ihre Email-Adresse an."
             maxlength="100"
-            required="true">
-        </div>
-        <!-- benutzername -->
-        <div class="form-group">
-          <label for="username">Benutzername *</label>
-          <input type="text" name="username" class="form-control" id="username"
-            value="<?php echo $username ?>"
-            placeholder="Gross- und Keinbuchstaben, min 6 Zeichen."
-            pattern="(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,}"
-            title="Gross- und Keinbuchstaben, min 6 Zeichen."
-            maxlength="30" 
             required="true">
         </div>
         <!-- password -->
