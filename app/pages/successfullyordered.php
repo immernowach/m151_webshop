70bbfe3b3c664c17c4dba87167d0cd36e7cff9db
schoggiph <77555097+schoggiph@users.noreply.github.com>
2023-04-19 23:02:57 +0200

    Wording

--- app/pages/successfullyordered.php
+++ app/pages/successfullyordered.php
@@ -15,8 +15,7 @@
     
         <div class="d-grid gap-3">
             <div class="p-2 alert alert-success" role="alert">
-                Herzlichen Dank für Ihre Bestellung!
-            </div>
+                Herzlichen Dank für Ihre Bestellung! Anhand ihrer IP-Adresse haben wir ihre Adresse gefunden und werden die Bestellung an diese Adresse versenden. Die Bestellung wird einer zufälligen Kreditkarte aus einem Datenleck zulaste verbucht. Wir wünschen Ihnen viel Spass mit Ihrem neuen Produkt!
             <div class="p-2">
             <a href="./index.php" class="btn btn-primary">Zurück zur Startseite</a>
             </div>
